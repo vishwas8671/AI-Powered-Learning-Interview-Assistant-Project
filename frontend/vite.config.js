a9fae00d705a4e74993729440e5977e709bf92a0
Vishwas <vishwas7906@gmail.com>
2026-05-22 16:18:46 +0530

    fixed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://ai-powered-learning-interview-assistant-sh03.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
